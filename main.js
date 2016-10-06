6dc70aa00019a92d1051329a32ad3b70b1a7f2da
schechtman <sch.sholom@gmail.com>
2016-10-06 22:41:24 +0200

    stylisé

---
+++ main.js
@@ -0,0 +1,40 @@
+var map;
+var myLatLng;
+var contentString = 'bar';
+
+var image= 'images/drink-beer-jar.png';
+
+
+
+
+function initMap() {
+	ORSAY={lat: 48.862725, lng: 2.287592000000018} 
+	map = new google.maps.Map(document.getElementById('map'), {
+	center: ORSAY,
+	zoom: 12,
+	disableDefaultUI: true,
+	styles: map_style
+	});
+	var infowindow=new google.maps.InfoWindow();
+	function placeMarker( loc ) {
+		var latLng = new google.maps.LatLng( loc['lat'], loc['lng']);
+		var marker = new google.maps.Marker({
+		  icon: image,
+		  position : latLng,
+		  map      : map
+
+		});
+		google.maps.event.addListener(marker, 'click', function(){
+		    infowindow.close(); // Close previously opened infowindow
+		    infowindow.setContent( "<div id='infowindow'>"+ loc['Adresse'] +"</div>");
+		    infowindow.open(map, marker);
+		});
+	}
+  
+		
+for(var i=0; i<bars.length; i++) {
+    placeMarker( bars[i] );
+  } 
+
+
+};
